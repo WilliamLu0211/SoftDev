f15cc0a23168f68107820207f59b7b2c4586731b
William Lu <rlu1@stuy.edu>
2018-10-01 19:36:45 -0400

    solo work

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,12 +1,18 @@
+# William Lu
+# SoftDev1 pd7
+# K14 -- Do I Know You?
+# 2018-10-02
+
 from flask import Flask, request, render_template, session, redirect, url_for
 from os import urandom
 
 app = Flask(__name__)
+
 app.secret_key = urandom(32)
 
 users = {"123":"qwer"}
 
-print(app.secret_key)
+# print(app.secret_key)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -16,26 +22,28 @@ def index():
         usr = request.form.get("username")
 
         if usr not in users.keys() or users[usr] != request.form.get("password"):
-            return redirect("/login")
+            return login()
 
         elif users[usr] == request.form.get("password"):
             session["username"] = usr
             return render_template("index.html", a = False, username = usr)
-        
-        
 
 @app.route("/login", methods=["POST"])
 def login():
     usr = request.form.get("username")
-    
+
     if usr not in users.keys():
         return render_template("error.html", msg = "Username not found.")
-    
+
     elif users[usr] != request.form.get("password"):
         return render_template("error.html", msg = "Incorrect password.")
 
     else:
         return render_template("error.html", msg = "Uh we screwed up")
+
+@app.route("/out")
+def logout():
+    return redirect("static/logout.html")
 
 if __name__ == "__main__":
     app.debug = True
