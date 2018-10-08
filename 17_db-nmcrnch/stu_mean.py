5c601f7ab18e92072b298589254c79ed6afc4a15
William Lu <rlu1@stuy.edu>
2018-10-08 10:32:15 -0400

    ,

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -12,31 +12,30 @@ c = db.cursor()               #facilitate db ops
 
 c.execute('CREATE TABLE peeps_avg (id INTEGER, avg REAL)')
 
-data = c.execute("SELECT * FROM peeps, courses WHERE peeps.id == courses.id;")
+# Data returned from DB in format (name, age, id, course_name, grade, id)
 
-idDict = {}
-nameDict = {}
+data = c.execute("SELECT * FROM peeps, courses WHERE peeps.id = courses.id;")
+
+grade_dict = {}
+
+# Stores data as {id: (name, [grades])}
+
 for row in data:
-    if row[2] in idDict.keys():
-        idDict[row[2]].append(row[5])
+    if row[2] in grade_dict.keys():
+        grade_dict[row[2]][1].append(row[4])
     else:
-        idDict[row[2]] = [row[5]]
-    if row[1] not in nameDict.keys():
-        nameDict[row[1]] = row[2]
+        grade_dict[row[2]] = (row[0], [row[4]])
 
-# print(idDict)
-print('name,id,average')
+print('name, id, average')
 
-for name in nameDict:
-    sum = 0
-    ctr = 0
-    id = nameDict[name]
-    for mark in idDict[id]:
-        sum += mark
-        ctr += 1
-    avg = sum / float(ctr)
-    print(name + ',' + str(id) + ',' + str(avg))
-    c.execute('INSERT INTO peeps_avg VALUES (' + str(id) + ', ' + str(avg) + ');')
+for student_id in grade_dict:
+    grades = grade_dict[student_id][1]
+    avg = float(sum(grades)/len(grades))
+    print("%s, %s, %s" % (grade_dict[student_id][0], student_id, avg))
+    c.execute('INSERT INTO peeps_avg VALUES (%s , %s);' % (student_id, str(avg)))
+
+def add_course(name, grade, student_id):
+    c.execute('INSERT INTO courses VALUES (\"%s\", %s, %s);' % (name, grade, student_id))
 
 db.commit() #save changes
 db.close()  #close database
