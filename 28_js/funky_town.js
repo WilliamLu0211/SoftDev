fc053665bcaedadfdb31d77d4c67a962b3458852
William Lu <rlu1@stuy.edu>
2018-12-18 18:27:56 -0500

    |

---
+++ 28_js/funky_town.js
@@ -0,0 +1,25 @@
+// William Lu
+// SoftDev1 pd7
+// K28 -- Sequential Progression
+// 2018-12-19 W
+
+var fibonacci = (n) => {
+  if (!n)
+    return 0;
+  if (n == 1)
+    return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+var gcd = (a, b) => {
+  if (!b)
+    return a;
+  return gcd(b, a % b); //reverses the order of a and b
+}
+
+var studentList = ["adayR", "aschJ", "belkebirI", "chenJ", "chowdhuryJ", "cwalinaP", "gondalA", "guptaS", "hasanA", "johalP", "keriazisD", "liK", "linJ", "linV", "liuA", "luW", "maiJ", "mohriC", "narangA", "ngR", "onishiR", "peciR", "petersT", "rachlevskyM", "wongT", "wuR", "yeJ", "zhangI", "zhaoM", "zhouQ"];
+
+var randomStudent = () => {
+  var index = parseInt(Math.random() * studentList.length);
+  return studentList[index];
+}
